0c22554aedce78fef2c1e2d1bdcfd3065c93f3ce
giomenezes <giovanna.rodrigues@sptech.school>
2023-10-24 08:39:02 -0300

    fix(): arrumando identação

--- gct-script.sh
+++ gct-script.sh
@@ -1,23 +1,77 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y #atualizando pacotes
 
-java -version #verificando versao atual do java
-if [ $? = 0 ]; #se retorno for igual a 0
-then #entao,
-echo “Cliente possui java instalado” #print no terminal
-else #se nao,
-echo “Cliente não possui java instalado” #print no terminal
-echo “Gostaria de instalar o java? [s/n]” #print no terminal
-read get #variável que guarda resposta do usuário
-if [ \“$get\” == \“s\” ]; #se retorno for igual a s
-then #entao
-sudo apt install openjdk-17-jre -y #executando instalacao do java
-echo "Java instalado com sucesso!"
-else {
-echo "Cancelando instalação do Java"
-}
-fi #fechando o 2º if
-fi #fechando o 1º if
+# Config.
+versao_recomendada_java="17.0.6"
+
+echo " ██████╗ ██████╗ ███████╗██╗   ██╗"
+echo "██╔════╝ ██╔══██╗██╔════╝╚██╗ ██╔╝"
+echo "██║  ██╗ ██████╔╝█████╗   ╚████╔╝"
+echo "██║  ╚██╗██╔══██╗██╔══╝    ╚██╔╝"
+echo "╚██████╔╝██║  ██║███████╗   ██║"
+echo " ╚═════╝ ╚═╝  ╚═╝╚══════╝   ╚═╝"
+echo ""
+echo " █████╗ ██╗      █████╗ ██╗   ██╗██████╗"
+echo "██╔══██╗██║     ██╔══██╗██║   ██║██╔══██╗"
+echo "██║  ╚═╝██║     ██║  ██║██║   ██║██║  ██║"
+echo "██║  ██╗██║     ██║  ██║██║   ██║██║  ██║"
+echo "╚█████╔╝███████╗╚█████╔╝╚██████╔╝██████╔╝"
+echo " ╚════╝ ╚══════╝ ╚════╝  ╚═════╝ ╚═════╝"
+echo ""
+echo "████████╗██████╗  █████╗ ███╗  ██╗ ██████╗ █████╗  █████╗ ████████╗██╗ █████╗ ███╗  ██╗ ██████╗"
+echo "╚══██╔══╝██╔══██╗██╔══██╗████╗ ██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██║██╔══██╗████╗ ██║██╔════╝"
+echo "   ██║   ██████╔╝███████║██╔██╗██║╚█████╗ ███████║██║  ╚═╝   ██║   ██║██║  ██║██╔██╗██║╚█████╗ "
+echo "   ██║   ██╔══██╗██╔══██║██║╚████║ ╚═══██╗██╔══██║██║  ██╗   ██║   ██║██║  ██║██║╚████║ ╚═══██╗"
+echo "   ██║   ██║  ██║██║  ██║██║ ╚███║██████╔╝██║  ██║╚█████╔╝   ██║   ██║╚█████╔╝██║ ╚███║██████╔╝"
+echo "   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚══╝╚═════╝ ╚═╝  ╚═╝ ╚════╝    ╚═╝   ╚═╝ ╚════╝ ╚═╝  ╚══╝╚═════╝ "
+
+linha="=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+
+echo $linha
+echo "                             [@] SCRIPT DE INSTALAÇÃO"
+echo "                             [@] Equipe GCT - 1° CCO "
+echo $linha
+
+echo ""
+
+echo "[!] Fazendo verificação do Java"
+which java | grep /usr/bin/java
+
+if [ $? = 0 ]; then
+	versao_java=$(java -version 2>&1 | awk -F'"' '{print $2}')
+        echo "[!] Versão do Java Instalada: $versao_java"
+        
+        if [ "$versao_java" = $versao_recomendada_java ]; then 
+		echo "[!] Esta versão está de acordo com a API da GCT."
+		echo "[!] Finalizando o Script."
+            else    
+                echo "[ALERT] Esta versão NÃO é adequada para a API da GCT."
+                echo "[?] Deseja atualizar para a versão recomendada? [s/n]"
+                
+                read escolha
+                
+                if [ $escolha = "s" ]; then
+		        sudo apt-get install openjdk-17-jre -y
+		        echo "[!] O Java foi atualizado com sucesso."
+		        echo "[!] Finalizando o script"
+                    else
+                        echo "[!] Finalizando o script de instalação!"
+		fi
+		fi
+	else
+		echo ""
+		echo "[ALERT] O Java não está instalado"
+		echo "[?] Deseja Instalar o Java? [s/n]"
+		
+		read get
+		
+		if [ $get = "s" ]; then
+			sudo apt-get install openjdk-17-jre -y
+			echo "[!] O Java foi  Instalado!"
+		else
+			echo "Cancelando instalação"
+		fi
+fi
 
 echo "Inicializando NMON"
 nmon
@@ -27,3 +81,7 @@ cd /home/ubuntu/CapturasNMON #alterando para a pasta criada
 nmon -f -s 5 -c 10 #capturando  dados, com leituras a cada 5 segundos, 10 vezes em s>
 
 wget https://raw.githubusercontent.com/MarcioIoT/NmonVisualizer/main/NMONVisualizer.jar #instalando visualizador de dados capturados via NMON
+
+
+
+
